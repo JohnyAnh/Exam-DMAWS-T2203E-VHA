06f0d8d7b11fdf35bfbf4d06d1d2258811a6a892
JohnyAnh <112863282+JohnyAnh@users.noreply.github.com>
2023-07-22 21:28:30 +0700

    RS

--- DMAWS_T2203E_VUHOANGANH/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DMAWS_T2203E_VUHOANGANH.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
-
